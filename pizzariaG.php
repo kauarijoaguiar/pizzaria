66b64a9a58706d4a01f6e6d682bcfaebf6104ca8
juliapcp <juliapcp2003@gmail.com>
2021-11-07 17:03:38 -0300

    feita a g

--- pizzariaG.php
+++ pizzariaG.php
@@ -7,24 +7,18 @@
 </head>
 <body>
 <?php
-/*
-	select ingrediente.nome as nomeingrediente from ingrediente
-	join saboringrediente on saboringrediente.ingrediente=ingrediente.codigo
-	join sabor on saboringrediente.sabor=sabor.codigo
 
-*/
+
 function url($campo, $valor) {
 	$result = array();
 	if (isset($_GET["tamanho"])) $result["tamanho"] = "tamanho=".$_GET["tamanho"];
 	if (isset($_GET["borda"])) $result["borda"] = "borda=".$_GET["borda"];
-	if (isset($_GET["sabores"])) $result["sabores"] = "sabores=".$_GET["sabores"];
 	if (isset($_GET["valor"])) $result["valor"] = "valor=".$_GET["valor"];
-	if (isset($_GET["total"])) $result["total"] = "total=".$_GET["total"];
 
 	if (isset($_GET["orderby"])) $result["orderby"] = "orderby=".$_GET["orderby"];
 	if (isset($_GET["offset"])) $result["offset"] = "offset=".$_GET["offset"];
 	$result[$campo] = $campo."=".$valor;
-	return("pizzariaA.php?".strtr(implode("&", $result), " ", "+"));
+	return("pizzariaG.php?numero=".$_GET["numero"]."&".strtr(implode("&", $result), " ", "+"));
 }
 
 $db = new SQLite3("pizzaria.db");
@@ -32,7 +26,7 @@ $db->exec("PRAGMA foreign_keys = ON");
 
 $limit = 5;
 
-echo "<h1>Pizzas da Comanda ALGUMA COISA</h1>\n";
+echo "<h1>Pizzas da Comanda " . $_GET["numero"]."</h1>\n";
 
 $value = "";
 if (isset($_GET["tamanho"])) $value = $_GET["tamanho"];
@@ -53,9 +47,7 @@ echo "<td><b>Tamanho</b> <a href=\"".url("orderby", "tamanho+asc")."\">&#x25BE;<
 echo "<td><b>Borda</b> <a href=\"".url("orderby", "borda+asc")."\">&#x25BE;</a> <a href=\"".url("orderby", "borda+desc")."\">&#x25B4;</a></td>\n";
 echo "<td><b>Sabores</b></td>\n";
 echo "<td><b>Valor</b> <a href=\"".url("orderby", "valor+asc")."\">&#x25BE;</a> <a href=\"".url("orderby", "valor+desc")."\">&#x25B4;</a></td>\n";
-echo "<td><b>Total</b></td>\n";
 
-echo "<td></td>\n";
 echo "</tr>\n";
 
 
@@ -66,13 +58,13 @@ $offset = (isset($_GET["offset"])) ? max(0, min($_GET["offset"], $total-1)) : 0;
 $offset = $offset-($offset%$limit);
 
 $results = $db->query(
-"select numero, pizza.codigo, group_concat(sabor.nome, ',') as sabor, sabor.codigo as codigo, case
+	"select numero, pizza.codigo, group_concat(sabor.nome, ',') as sabor, sabor.codigo as codigo, case
 when pizza.borda is null then 'não'
 else borda.nome
-end as borda, max(case
+end as borda,'R$ ' || REPLACE(max(case
 when borda.preco is null then 0
 else borda.preco
-end+precoportamanho.preco) || ' $' as valor, tamanho.nome as tamanho from comanda
+end+precoportamanho.preco), '.', ',') as valor, tamanho.nome as tamanho from comanda
 join pizza on pizza.comanda = comanda.numero
 join pizzasabor on pizza.codigo = pizzasabor.pizza
 join sabor on pizzasabor.sabor = sabor.codigo 
@@ -97,10 +89,13 @@ while($row = $results->fetchArray()){
 	echo "<td>";
 	echo $row["valor"];
 	echo "</td>";
-
-	echo "<td>\n";
-    $results2 = $db->query(
-		"select sum(tmp.preco) as total
+	echo "</tr>\n";
+}
+echo "<tr>";
+echo "<td colspan=3><b>Total</b></td>\n";
+echo "<td><b>";
+$results2 = $db->query(
+	"select sum(tmp.preco) as total
 	from
 		(select
 			max(case
@@ -113,22 +108,16 @@ while($row = $results->fetchArray()){
 			join sabor on pizzasabor.sabor = sabor.codigo
 			join precoportamanho on precoportamanho.tipo = sabor.tipo and precoportamanho.tamanho = pizza.tamanho
 			left join borda on pizza.borda = borda.codigo
-		where comanda.numero = ".$_GET["numero"]." group by pizza.codigo) as tmp;");
-    while ($row2 = $results2->fetchArray()){
-        echo $row2["total"];
-    }
-        echo "</td>\n";
-
-
-	echo "</tr>\n";
+		where comanda.numero = " . $_GET["numero"] . " group by pizza.codigo) as tmp;"
+);
+while ($row2 = $results2->fetchArray()) {
+	echo 'R$ ' . str_replace(".", ",", $row2["total"]);
 }
-
-
-echo "<td><b>Total</b></td>\n";
+echo "</b></td>";
+echo "</tr>";
 echo "</table>\n";
-echo "<td><a href=\"pizzariaD.php\"><button>Voltar</button></a></td>";
 
-echo "<br>\n";
+echo "<a href=\"pizzariaD.php\"><button>Voltar</button></a>";
 
 $db->close();
 ?>
